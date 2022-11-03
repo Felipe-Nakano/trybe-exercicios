5e70f437c9f81db628b473ec9a4a99f56f08d79c
Felipe-Nakano <felipenakano04@gmail.com>
2022-11-02 21:16:34 -0300

    .

---
+++ introducao-ao-js/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/node.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let primos = 0;
+
+for (let index =0; index > numbers.length; index+=1){
+    if (numbers[index] / number[index] && 1) {
+        primos +=1
+    } else {
+
+    }
+}
+console.log(primos);
